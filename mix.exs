c2efcf7f9f21a9b7ee4334a221776f497c260439
Gregory McIntyre <greg@gregorymcintyre.com>
2017-08-02 15:45:01 +1000

    Update deps

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule GameServer.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.7"},
      {:uuid, "~> 1.1"}]
   end
 end
